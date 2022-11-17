79bfeb4b646b51bdd82668bf43a7c9b930069707
Gregory Jans <gjans0430@floridapoly.edu>
2022-11-16 20:51:21 -0500

    * Added task 2 to learn grep * Added fake player files in TrappedUsers dir for future use * put some ideas in main file

--- reset.sh
+++ reset.sh
@@ -6,9 +6,29 @@
 # always reset before pushing changes 
 
 if [ -d TrappedUsers ]; then 
-    rmdir TrappedUsers
+    mv TrappedUsers .TrappedUsers
 fi
 
 if [ -f note ]; then
     rm note
+fi
+
+for i in {2..7}
+do
+    if [ -d "level$i" ]; then 
+        mv "level$i" ".level$i"
+    fi
+done
+
+if [ -f "TrappedUsers/$UNAME.player" ]; then
+    rm "TrappedUsers/$UNAME.player"
+fi
+
+if [ ! -f .level6/note ]; then 
+    touch .level6/note
+    echo "I'm glad you could find my next note" >> .level6/note
+    echo "I've been snooping around too and the file 'rules' seems unusually cluttered" >> .level6/note
+    echo "Maybe we can figure out a way to filter the text and look for some kind of password or key" >> .level6/note
+    echo >> .level6/note
+    echo "Dont forget: you can run any .sh file by typing ./[filename]" >> .level6/note
 fi
